1ae763a25a906de452457c20246b55d64e4f8d47
Гліб Подлипський <hyperlivegroup@gmail.com>
2024-10-30 11:13:10 +0000

    add basic setup

---
+++ client/src/pages/auth/AuthPage.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss'
+
+function AuthPage() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
+
+export default AuthPage;
